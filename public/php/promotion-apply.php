96054360ff127a5c42d468a75822b3b4ae3edccf
Mogria <m0gr14@gmail.com>
2019-03-14 23:08:10 +0100

    move tokens from promotions.xml into database/promoTokens.xml to keep them secret

--- public/php/promotion-apply.php
+++ public/php/promotion-apply.php
@@ -2,41 +2,56 @@
     include __DIR__ . '/../../lib/xmlutils.php';
     //error_reporting(0);
     $xml = simplexml_load_file('../promotions.xml');
+    $tokens_xml = simplexml_load_file('../../database/promoTokens.xml');
     $out = $object = (object) [
         'message' => '',
         'type' => 'error'
     ];
 
     $promotion = insertIntoXML($xml);
+    $token = insertIntoTokenXML($tokens_xml, $promotion->attributes()->id);
 
     $xml_new_valid = validateXML($xml, '../schemas/promotions.xsd');
+    $tokens_xml_new_valid = validateXML($xml, '../schemas/promotions.xsd');
 
     #store xml into original xml if validation is ok
-    if($xml_new_valid){
+    if($xml_new_valid && $tokens_xml_new_valid){
         persistXML('../promotions.xml', $xml);
+        persistXML('../../database/promoTokens.xml', $tokens_xml);
         $object->message = <<<EOT
 Promotion wurde erfolgreich erstellt.
 
 Ihr Zugriffstoken für diese Promotion (bitte speichern):
 
 EOT;
-        $object->message .= $promotion["token"];
+        $object->message .= (string)$token;
         $object->type = "success";
     }
     else{
         $object->message = "validation failed";
     }
 
-    function generateToken() {
-        // secure random number generation, 128 bit.
+    function insertIntoTokenXML($xml, $promo_id) {
+        // secure random number generation, 64 bit.
         // this is not feasibly brute-forcable, not even by
         // this application itself and won't collide
-        return bin2hex(random_bytes(32));
+        $token = $xml->addChild('token', bin2hex(random_bytes(16)));
+        $token->addAttribute('promotion-id', $promo_id);
+        return $token;
+    }
+
+    function generateNewId($xml) {
+        $xml->registerXPathNamespace('p', 'http://wgcard.xml.hslu.ch/promotions');
+        $ids = array_map(function($e) {
+            return (int)$e->id;
+        }, $xml->xpath("//p:promotion/@id"));
+        return $ids ? max($ids) + 1 : 1;
     }
 
     function insertIntoXML($xml) {
         $promotion = $xml->addChild('promotion', '');
-        $promotion->addAttribute('token', generateToken());
+
+        $promotion->addAttribute('id', generateNewId($xml));
         $promotion->addAttribute('added', date('Y-m-d'));
         $promotion->addChild('name', $_POST['name']);
         $promotion->addChild('description', $_POST['description']);

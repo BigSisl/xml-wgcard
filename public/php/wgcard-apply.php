c525d1eeea83ff29e1b78bc0c63f38c2a3899836
Mogria <m0gr14@gmail.com>
2019-03-15 20:18:39 +0100

    add barcode for each WG to secret file database/barcodes.xml

--- public/php/wgcard-apply.php
+++ public/php/wgcard-apply.php
@@ -2,18 +2,31 @@
     include __DIR__ . '/../../lib/xmlutils.php';
     //error_reporting(0);
     $xml = simplexml_load_file('../wgs.xml');
+    $barcodes_xml = simplexml_load_file('../../database/barcodes.xml');
 
-    insertIntoXML($xml);
+    $wg = insertIntoXML($xml);
+    insertIntoTokenXML($barcodes_xml, $wg->attributes()->id);
 
     $xml_new_valid = validateXML($xml, '../schemas/wgs.xsd');
+    $barcodes_xml_new_valid = validateXML($barcodes_xml, '../schemas/barcodes.xsd');
 
     #store xml into original xml if validation is ok
-    if($xml_new_valid){
+    if($xml_new_valid && $barcodes_xml_new_valid){
         persistXML('../wgs.xml', $xml);
+        persistXML('../../database/barcodes.xml', $barcodes_xml);
         echo "wg was successfully added";
     }
     else{
         echo "validation failed";
+    }
+
+    function insertIntoTokenXML($xml, $wg_id) {
+        // secure random number generation, 64 bit.
+        // this is not feasibly brute-forcable, not even by
+        // this application itself and won't collide
+        $barcode = $xml->addChild('barcode', bin2hex(random_bytes(8)));
+        $barcode ->addAttribute('wg-id', $wg_id);
+        return $barcode;
     }
 
     function generateNewId($xml) {
@@ -40,6 +53,7 @@
         $address = $wg->addChild('address', '');
 
         insertWgAddress($address);
+        return $wg;
     }
     function insertWgMember($wg_member, $i){
         $person = $wg_member->addChild('person', '');
